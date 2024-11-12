7f9f0036c47b1c7f4a28b41dc2db461df3d722a8
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-12 23:31:07 +0200

    189. Rotate Array

---
+++ array/189.rotate_array.ts
@@ -0,0 +1,37 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+ function rotate(nums: number[], k: number): void {
+   k=k%nums.length;// to make sure that we arenot making rotation not required (if lenght=4,k=4  then the array will be the same )
+   reverse(nums,k);
+   reverseFirstKElemnt(nums,k);
+   reverseRemaningElemnt(nums,k);
+  };
+   
+  const reverse =(nums:number[],k:number)=>{
+      let l=0;
+      let r=nums.length-1;
+      while(r>l){
+          [nums[r],nums[l]]=[nums[l],nums[r]];
+           l++;
+          r--;
+      }
+  }
+  const reverseFirstKElemnt=(nums:number[],k:number)=>{
+      let l=0;
+      let r=k-1;
+      while(r>l){
+          [nums[r],nums[l]]=[nums[l],nums[r]];
+          l++;
+          r--;
+      }
+  }
+  const reverseRemaningElemnt=(nums:number[],k:number)=>{
+      let l=k;
+      let r=nums.length-1;
+      while(r>l){
+          [nums[r],nums[l]]=[nums[l],nums[r]];
+          l++;
+          r--;
+      }
+  }
