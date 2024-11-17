09b46013bfa9b14d268b19fd186ec297d5ec6a12
Yehia Mohamed <yehiamohamed915@yahoo.com>
2024-11-17 17:24:49 +0200

    [200~14. Longest Common Prefix

---
+++ array/14.longest_common_prefix.ts
@@ -0,0 +1,9 @@
+function longestCommonPrefix(strs: string[]): string {
+   let longestPrefix=strs[0];
+   for(let str of strs){
+       while(!str.startsWith(longestPrefix)){
+           longestPrefix=longestPrefix.substring(0,longestPrefix.length-1)
+       }
+   }
+   return longestPrefix;
+};
